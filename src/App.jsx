67caf42858badf839025577ea59bfb10210dde61
Stanislav Svoboda <52444330+nuclearkofola@users.noreply.github.com>
2025-08-03 23:08:14 +0200

    Opravdu funkční Theme control

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,10 @@ function App() {
   return (
     <>
     <div>
-<input type="checkbox" value="synthwave" className="checkbox theme-controller" />
-      
-<h1 className="text-3xl font-bold">Vite + React</h1>
-
+      <h1 className="text-3xl font-bold">Vite + React</h1>
 <label className="swap swap-rotate">
   {/* this hidden checkbox controls the state */}
-  <input type="checkbox" className="theme-controller" value="synthwave" />
+  <input type="checkbox" className="theme-controller" value="dark" />
 
   {/* sun icon */}
   <svg
@@ -33,9 +30,8 @@ function App() {
       d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
   </svg>
 </label>
+
     </div>
-
-
     </>
   )
 }
